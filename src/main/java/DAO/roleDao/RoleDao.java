3ff41a8ed75c2abedc29be5bc209ee65e8565fd8
Mustafetov Beknur <mustafetovbr@gmail.com>
2020-11-26 17:15:16 +0300

    init

---
+++ src/main/java/DAO/roleDao/RoleDao.java
@@ -0,0 +1,12 @@
+package DAO.roleDao;
+
+import model.Role;
+
+import java.util.Collection;
+
+public interface RoleDao {
+    void addRole(String role);
+    Role getRoleById( long id);
+    Collection<Role> getAllRoles();
+    boolean deleteRoleById(long id);
+}
